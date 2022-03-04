27daa329f2265db5d9512f5846c69cac7bf9b15e
Vakulina <viktoria.ganina@yandex.ru>
2022-03-04 13:22:54 +0300

    сделать раскладку лиг на гридах

--- src/components/Grid/GridList.js
+++ src/components/Grid/GridList.js
@@ -1,40 +1,44 @@
 import * as React from 'react';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
+
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import LeagesCard from '../LeagesCard/LeagesCard';
+import { Pagination, Stack, Typography } from '@mui/material';
 
 
 
-  export default function GridList() {
-    const navigate = useNavigate();
-    const actionHandler = (item) => {
-      navigate(`/leages/${item.id}`)
-    };
-  
-    const Item = styled(Paper)(({ theme }) => ({
-  
-    }));
-    return (
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={{ xs: 6, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {Array.from(Array(15)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>xs=2</Item>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    );
-          }
+export default function GridList({ listItems }) {
+  const navigate = useNavigate();
 
-          /*
-          listItems.map((item) => (
-        
-  <div onClick={actionHandler.bind(this, item)} key={item.id} >
-    <LeagesCard card={item} />
-  </div>
+  const actionHandler = (item) => {
+    navigate(`/leages/${item.id}`)
+  };
+
+  const Item = styled(Paper)(({ theme }) => ({
+
+  }));
+  return (
+    <Stack spacing={2}>
+      <Grid container spacing={{ xs: 6, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(listItems).map((item) => (
+          <Grid item xs={2} sm={4} md={4} key={item.id} >
+            <div onClick={actionHandler.bind(this, item)} key={item.id} >
+              <LeagesCard card={item}/>
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+      <Pagination count={10} variant="outlined" shape="rounded" />
+    </Stack>
+  );
+}
+
+/*
+listItems.map((item) => (   
+<div onClick={actionHandler.bind(this, item)} key={item.id} >
+<LeagesCard card={item} />
+</div>
 ))
 */
