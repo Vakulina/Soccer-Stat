a31c0d804e8b3feb94b66caa713bf48c838cd6c2
Vakulina <viktoria.ganina@yandex.ru>
2022-03-06 22:13:35 +0300

    перевести хлебные крошки на русский

--- src/components/BreadCrumbs/BreadCrumbsItem.js
+++ src/components/BreadCrumbs/BreadCrumbsItem.js
@@ -3,10 +3,11 @@ import { Breadcrumbs, Typography, Link } from '@mui/material';
 import { getTypeOfCompetitions } from '../../store/reducer';
 export default function BreadCrumbsItem({ name }) {
   const type = useSelector(getTypeOfCompetitions);
+  console.log(type)
   return (
     <Breadcrumbs separator="›" aria-label="breadcrumb">
       <Link underline="hover" color="inherit" href={`/${type}`}>
-        {type}
+        {(type==='leages') ? 'Лиги' : 'Команды'}
       </Link>
       <Typography color="text.primary">{name}</Typography>
     </Breadcrumbs>
