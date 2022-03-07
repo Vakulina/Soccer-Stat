f4034a32ca2ee252f416628139e04c2aa7e62877
Vakulina <viktoria.ganina@yandex.ru>
2022-03-07 23:26:22 +0300

    заменить регулярку

--- src/service/isValideDates.js
+++ src/service/isValideDates.js
@@ -1,15 +1,12 @@
 import moment from 'moment';
 import { RANGE_DATE_ERROR } from './errorsMessage';
-const regex1 = /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/ 
-const regex2 = /[0-9]{4}-[0-9]{2}-[0-9]{2}/   //регулярка нужна только для того, чтобы убедить, что введены ВСЕ цифры
-
+const regex = /([0-9]{2}\/[0-9]{2}\/[0-9]{4})||([0-9]{4}-[0-9]{2}-[0-9]{2})/ //проверяет что введены ВСЕ цифры в дате
 export default function isValideDates(start, end) {
+  const isValidStart = regex.test(start);
+  const isValidEnd = regex.test(end);
 
-  const isValidStart = regex1.test(start) || regex2.test(start)
-  const isValidEnd = regex1.test(end) || regex2.test(end)
-
-if(moment(start).isAfter(end) && isValidStart && isValidEnd ) {
-  throw new RangeError(RANGE_DATE_ERROR);
-}
-  return isValidStart&&isValidEnd;
+  if (moment(start).isAfter(end) && isValidStart && isValidEnd) {
+    throw new RangeError(RANGE_DATE_ERROR);
+  }
+  return isValidStart && isValidEnd;
 }
