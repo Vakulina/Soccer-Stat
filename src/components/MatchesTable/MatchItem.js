1a944a2e6f110d7113cf176670d4aaafb760471c
Vakulina <viktoria.ganina@yandex.ru>
2022-03-07 15:53:25 +0300

    добавить очистку дат после размонтирования таблицы матчей

--- src/components/MatchesTable/MatchItem.js
+++ src/components/MatchesTable/MatchItem.js
@@ -2,13 +2,23 @@ import moment from 'moment';
 import imageGoals from '../../service/imagingGoals';
 import { TableRow, TableCell } from '@mui/material'
 import './MatchItems.css'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setDateEnd, setDateStart } from '../../store/actions';
 
 export default function MatchItem(props) {
+  const dispatch = useDispatch();
   const {
     status, homeTeam, awayTeam, score, utcDate,
   } = props.data;
   const goals = imageGoals(score);
   const data = moment(utcDate).format('YYYY-MM-DD');
+  useEffect(()=>{
+    return function cleanup() {
+      dispatch(setDateEnd(false))
+      dispatch(setDateStart(false))
+    };
+  })
 
   return (
     <TableRow className='row'>
